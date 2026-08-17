600dfc164ec00f2cea7bb5582aa036eb413bd177
Jhustina <125208498+JhustinniaConfusionada@users.noreply.github.com>
2026-08-18 03:09:32 +0800

    font applyd

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 import App from './App';
 import '../styles.css';
 
