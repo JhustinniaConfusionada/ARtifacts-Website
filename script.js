07a5773a1c0761a0e4786b449b9f0489cfe3a61c
Jhustina <125208498+JhustinniaConfusionada@users.noreply.github.com>
2026-08-21 12:41:52 +0800

    footer

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ export const setupScrollAnimation = () => {
         }
       });
     },
-    { threshold: 0.1 }
+    { threshold: 0.5 }
   );
 
   animateItems.forEach((item) => observer.observe(item));
